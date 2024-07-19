dc53e9c5de9137e2df9691731a5a2c2b6d2e8f63
Elena Natterer <elena.natterer@gmail.com>
2024-07-19 20:09:38 +0200

    integrated gradient accumulation

--- scripts/gnn_architectures.py
+++ scripts/gnn_architectures.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset, Subset
 import torch_geometric
 from torch_geometric.data import Data, Batch
+from sklearn.metrics import r2_score
 
 from torch_geometric.nn import PointNetConv
 from tqdm import tqdm
@@ -86,6 +87,88 @@ class MyGnn(torch.nn.Module):
             
         x = self.output_layer(x, edge_index)
         return x
+    
+    
+class MyGnnHardCoded(torch.nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, hidden_size: int, output_layer: str = 'gcn'):
+        """
+        in_channels: number of input features
+        out_channels: number of output features
+        hidden_size: size of hidden layer
+        gat_layers: number of GAT layers
+        heads: number of attention heads
+        gcn_layers: number of GCN layers
+        output_layer: 'gat' or 'gcn'
+        """
+        super().__init__()
+        
+        # Hyperparameters 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_size = hidden_size
+        self.output_layer = output_layer
+        self.graph_layers = []
+        layers = []
+        
+        # Architecture
+        local_MLP_1 = nn.Sequential(
+            nn.Linear(in_channels, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+        )
+        
+        global_MLP_1 = nn.Sequential(
+            nn.Linear(hidden_size, int(hidden_size/2)),
+            nn.ReLU(),
+            nn.Linear(int(hidden_size/2), int(hidden_size*2)),
+            nn.ReLU(),
+            nn.Linear(int(hidden_size*2), hidden_size)
+        )
+        
+        self.pointLayer = PointNetConv(local_nn = local_MLP_1, global_nn = global_MLP_1)
+                
+        layers.append((torch_geometric.nn.GATConv(hidden_size, int(hidden_size*2)), 'x, edge_index -> x'))
+        layers.append(nn.ReLU(inplace=True))
+        
+        layers.append((torch_geometric.nn.GATConv(int(hidden_size*2), int(hidden_size/2)), 'x, edge_index -> x'))
+        layers.append(nn.ReLU(inplace=True))
+        
+        layers.append((torch_geometric.nn.GATConv(int(hidden_size/2), int(hidden_size*2)), 'x, edge_index -> x'))
+        layers.append(nn.ReLU(inplace=True))
+        
+        layers.append((torch_geometric.nn.GATConv(int(hidden_size*2), hidden_size), 'x, edge_index -> x'))
+        layers.append(nn.ReLU(inplace=True))
+        
+        layers.append((torch_geometric.nn.GATConv(hidden_size, hidden_size), 'x, edge_index -> x'))
+        layers.append(nn.ReLU(inplace=True))
+        
+        
+        # for _ in range(gcn_layers):
+        #     layers.append((torch_geometric.nn.GCNConv(hidden_size, hidden_size), 'x, edge_index -> x'))
+        #     layers.append(nn.ReLU(inplace=True))
+
+        # Create the Sequential module with the layers
+        if layers:
+            self.graph_layers = torch_geometric.nn.Sequential('x, edge_index', layers)
+                    
+        if output_layer == 'gcn':
+            self.output_layer = torch_geometric.nn.GCNConv(hidden_size, out_channels)
+        elif output_layer == 'gat':
+            self.output_layer = torch_geometric.nn.GATConv(hidden_size, out_channels)
+        else:
+            raise ValueError("Invalid output layer")
+            
+        print("Model initialized")
+        print(self)
+
+    def forward(self, data):
+        x = data.x
+        edge_index = data.edge_index
+        x = self.pointLayer(x, data.pos, edge_index)
+        if self.graph_layers:
+            x = self.graph_layers(x, edge_index)
+        x = self.output_layer(x, edge_index)
+        return x
 
 def validate_model_pos_features(model, valid_dl, loss_func, device):
     model.eval()
@@ -100,25 +183,28 @@ def validate_model_pos_features(model, valid_dl, loss_func, device):
     return val_loss / num_batches if num_batches > 0 else 0
 
 
-def train(model, config=None, loss_fct=None, optimizer=None, train_dl=None, valid_dl=None, device=None, early_stopping=None):
+def train(model, config=None, loss_fct=None, optimizer=None, train_dl=None, valid_dl=None, device=None, early_stopping=None, accumulation_steps:int=3):
     for epoch in range(config.epochs):
         model.train()
         actual_vals = []
         predictions = []
+        optimizer.zero_grad()
         for idx, data in tqdm(enumerate(train_dl)):
             input_node_features, targets = data.x.to(device), data.y.to(device)
-            optimizer.zero_grad()
             
             # Forward pass
             predicted = model(data.to(device))
             
-            actual_vals.extend(targets.detach().numpy())
-            predictions.extend(predicted.detach().numpy())
+            actual_vals.extend(targets.cpu().detach().numpy())
+            predictions.extend(predicted.cpu().detach().numpy())
             
             # Backward pass
             train_loss = loss_fct(predicted, targets)
             train_loss.backward()
             optimizer.step()
+            if (idx + 1) % accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
             
             wandb.log({"train_loss": train_loss.item(), "epoch": epoch, "step": idx})
             # print(f"epoch: {epoch}, step: {idx}, loss: {train_loss.item()}")
@@ -127,13 +213,12 @@ def train(model, config=None, loss_fct=None, optimizer=None, train_dl=None, vali
         predictions = np.array(predictions)
         
         # Calculate R^2
-        sst = ((actual_vals - actual_vals.mean()) ** 2).sum()
-        ssr = ((actual_vals - predictions) ** 2).sum()
-        r2 = 1 - ssr / sst
+        r2 = r2_score(actual_vals, predictions)
+        wandb.log({"r2": r2, "epoch": epoch, "step": idx})
 
         val_loss = validate_model_pos_features(model, valid_dl, loss_fct, device)
-        print(f"epoch: {epoch}, validation loss: {val_loss}, R^2: {r2}")
-        wandb.log({"loss": val_loss, "epoch": epoch, "r2": r2})
+        print(f"epoch: {epoch}, validation loss: {val_loss}")
+        wandb.log({"loss": val_loss, "epoch": epoch})
             
         early_stopping(val_loss)
         if early_stopping.early_stop:
